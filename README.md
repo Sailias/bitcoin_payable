2dbb7caf2c476bcd7e54cdfd8743b7ac76a3d99e
Jonathan Salis <jtsseneca@hotmail.com>
2014-09-01 15:40:06 -0400

    Removing testnet config option as it's in the MPK now

--- README.md
+++ README.md
@@ -35,7 +35,6 @@ config/initializers/bitcoin_payable.rb
     BitcoinPayable.config.currency = :cad
     BitcoinPayable.config.node_path = "m/0/"
     BitcoinPayable.config.master_public_key = ENV["MASTER_PUBLIC_KEY"]
-    BitcoinPayable.config.testnet = false
 
 #### Node Path
 
--- lib/bitcoin_payable/config.rb
+++ lib/bitcoin_payable/config.rb
@@ -3,11 +3,10 @@ require 'singleton'
 module BitcoinPayable
   class Config
     include Singleton
-    attr_accessor :master_public_key, :node_path, :currency, :testnet, :open_exchange_key
+    attr_accessor :master_public_key, :node_path, :currency, :open_exchange_key
 
     def initialize
       @currency ||= :cad
-      @testnet ||= false
     end
   end
 end
