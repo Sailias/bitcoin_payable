84472ef27bc98eb2179525bfd4d68c1f81f81386
Jonathan Salis <jon@functionalimperative.com>
2015-10-18 09:27:03 -0400

    Skipping API calls for payments already paid in full

--- lib/bitcoin_payable/commands/payment_processor.rb
+++ lib/bitcoin_payable/commands/payment_processor.rb
@@ -13,31 +13,33 @@ module BitcoinPayable
         # => Preserve API calls
         check_paid(payment)
 
-        begin
-          adapter = BitcoinPayable::Adapters::Base.fetch_adapter
+        unless payment.paid_in_full?
+          begin
+            adapter = BitcoinPayable::Adapters::Base.fetch_adapter
 
-          adapter.fetch_transactions_for_address(payment.address).each do |tx|
-            tx.symbolize_keys!
+            adapter.fetch_transactions_for_address(payment.address).each do |tx|
+              tx.symbolize_keys!
 
-            unless payment.transactions.find_by_transaction_hash(tx[:txHash])
-              payment.transactions.create!(
-                estimated_value: tx[:estimatedTxValue],
-                transaction_hash: tx[:txHash],
-                block_hash: tx[:blockHash],
-                block_time: (Time.at(tx[:blockTime]) if tx[:blockTime]),
-                estimated_time: (Time.at(tx[:estimatedTxTime]) if tx[:estimatedTxTime]),
-                btc_conversion: payment.btc_conversion
-              )
+              unless payment.transactions.find_by_transaction_hash(tx[:txHash])
+                payment.transactions.create!(
+                  estimated_value: tx[:estimatedTxValue],
+                  transaction_hash: tx[:txHash],
+                  block_hash: tx[:blockHash],
+                  block_time: (Time.at(tx[:blockTime]) if tx[:blockTime]),
+                  estimated_time: (Time.at(tx[:estimatedTxTime]) if tx[:estimatedTxTime]),
+                  btc_conversion: payment.btc_conversion
+                )
 
-              payment.update(btc_amount_due: payment.calculate_btc_amount_due, btc_conversion: BitcoinPayable::CurrencyConversion.last.btc)
+                payment.update(btc_amount_due: payment.calculate_btc_amount_due, btc_conversion: BitcoinPayable::CurrencyConversion.last.btc)
+              end
             end
+
+            # => Check for payments after the response comes back
+            check_paid(payment)
+
+          rescue JSON::ParserError
+            puts "Error processing response from server.  Possible API issue or your Quota has been exceeded"
           end
-
-          # => Check for payments after the response comes back
-          check_paid(payment)
-
-        rescue JSON::ParserError
-          puts "Error processing response from server.  Possible API issue or your Quota has been exceeded"
         end
         
       end
