4d628387c8970c50c2f1380c5e492b307535d03e
Jonathan Salis <jon@functionalimperative.com>
2015-10-18 09:06:53 -0400

    Adding rescue to catch bad responses from server
    * This happens with Blockchain.info when you have exceeded
      your quota

--- lib/bitcoin_payable/commands/payment_processor.rb
+++ lib/bitcoin_payable/commands/payment_processor.rb
@@ -9,31 +9,49 @@ module BitcoinPayable
 
     def perform
       BitcoinPayable::BitcoinPayment.where(state: [:pending, :partial_payment]).each do |payment|
-        adapter = BitcoinPayable::Adapters::Base.fetch_adapter
+        # => Check for completed payment first, incase it's 0 and we don't need to make an API call
+        # => Preserve API calls
+        check_paid(payment)
 
-        adapter.fetch_transactions_for_address(payment.address).each do |tx|
-          tx.symbolize_keys!
+        begin
+          adapter = BitcoinPayable::Adapters::Base.fetch_adapter
 
-          unless payment.transactions.find_by_transaction_hash(tx[:txHash])
-            payment.transactions.create!(
-              estimated_value: tx[:estimatedTxValue],
-              transaction_hash: tx[:txHash],
-              block_hash: tx[:blockHash],
-              block_time: (Time.at(tx[:blockTime]) if tx[:blockTime]),
-              estimated_time: (Time.at(tx[:estimatedTxTime]) if tx[:estimatedTxTime]),
-              btc_conversion: payment.btc_conversion
-            )
+          adapter.fetch_transactions_for_address(payment.address).each do |tx|
+            tx.symbolize_keys!
 
-            payment.update(btc_amount_due: payment.calculate_btc_amount_due, btc_conversion: BitcoinPayable::CurrencyConversion.last.btc)
+            unless payment.transactions.find_by_transaction_hash(tx[:txHash])
+              payment.transactions.create!(
+                estimated_value: tx[:estimatedTxValue],
+                transaction_hash: tx[:txHash],
+                block_hash: tx[:blockHash],
+                block_time: (Time.at(tx[:blockTime]) if tx[:blockTime]),
+                estimated_time: (Time.at(tx[:estimatedTxTime]) if tx[:estimatedTxTime]),
+                btc_conversion: payment.btc_conversion
+              )
+
+              payment.update(btc_amount_due: payment.calculate_btc_amount_due, btc_conversion: BitcoinPayable::CurrencyConversion.last.btc)
+            end
           end
-        end
 
-        if payment.currency_amount_paid >= payment.price
-          payment.paid
-        elsif payment.currency_amount_paid > 0
-           payment.partially_paid
+          # => Check for payments after the response comes back
+          check_paid(payment)
+          
+        rescue JSON::ParserError
+          puts "Error processing response from server.  Possible API issue or your Quota has been exceeded"
         end
+        
       end
     end
   end
+
+  protected
+
+  def check_paid(payment)
+    if payment.currency_amount_paid >= payment.price
+      payment.paid
+    elsif payment.currency_amount_paid > 0
+       payment.partially_paid
+    end
+  end
+
 end
