7c8d83f9053bf010f6e44c372dd6e417cee579b5
Jonathan Salis <jon@voxpoplabs.com>
2018-03-08 19:27:20 -0500

    Removing old blocktrail adapter, cleaning up adapter base

--- lib/bitcoin_payable/adapters/base.rb
+++ lib/bitcoin_payable/adapters/base.rb
@@ -1,16 +1,14 @@
 module BitcoinPayable::Adapters
   class Base
 
-    class << self
-      def fetch_adapter
-        unless BitcoinPayable.config.adapter.blank?
-          api = BitcoinPayable.config.adapter
-          adapter_class = api.camelize + 'Adapter'
-          adapter_class = BitcoinPayable::Adapters.const_get(adapter_class)
-          adapter = adapter_class.new
-        else
-          raise "Please specify an adapter"
-        end
+    def self.fetch_adapter
+      unless BitcoinPayable.config.adapter.blank?
+        api = BitcoinPayable.config.adapter
+        adapter_class = api.camelize + 'Adapter'
+        adapter_class = BitcoinPayable::Adapters.const_get(adapter_class)
+        adapter = adapter_class.new
+      else
+        raise "Please specify an adapter"
       end
     end
 
