35795255739ebe99c42535af1eec3a9329ee340c
Jonathan Salis <jon@voxpoplabs.com>
2018-03-15 22:11:48 -0400

    Updating paid status only if not paid in full, making way for confirmations

--- lib/bitcoin_payable/interactors/transaction_processor/determine_payment_status.rb
+++ lib/bitcoin_payable/interactors/transaction_processor/determine_payment_status.rb
@@ -6,7 +6,7 @@ module BitcoinPayable::Interactors::TransactionProcessor
       fiat_paid = context.bitcoin_payment.currency_amount_paid
       
       if fiat_paid >= context.bitcoin_payment.price
-        context.bitcoin_payment.paid!
+        context.bitcoin_payment.paid! unless context.bitcoin_payment.paid_in_full?
       elsif fiat_paid > 0
         context.bitcoin_payment.partially_paid!
       end
