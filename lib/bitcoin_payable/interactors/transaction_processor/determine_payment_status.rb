28f6846f967ef1f54ab150728f3af4fc663be668
Jonathan Salis <jon@voxpoplabs.com>
2018-03-15 23:27:27 -0400

    Fixing partially paid transitions

--- lib/bitcoin_payable/interactors/transaction_processor/determine_payment_status.rb
+++ lib/bitcoin_payable/interactors/transaction_processor/determine_payment_status.rb
@@ -8,7 +8,7 @@ module BitcoinPayable::Interactors::TransactionProcessor
       if fiat_paid >= context.bitcoin_payment.price
         context.bitcoin_payment.paid! unless context.bitcoin_payment.paid_in_full?
       elsif fiat_paid > 0
-        context.bitcoin_payment.partially_paid!
+        context.bitcoin_payment.partially_paid! unless context.bitcoin_payment.partially_paid?
       end
     end
 
