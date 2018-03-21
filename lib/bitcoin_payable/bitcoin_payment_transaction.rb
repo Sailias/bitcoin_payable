40ea2cc47b18b3932e0f27525c750ae1e69e50d6
Jonathan Salis <jon@voxpoplabs.com>
2018-03-20 20:15:53 -0400

    Removing unused convert transaction from BitcoinPaymentTransaction

--- lib/bitcoin_payable/bitcoin_payment_transaction.rb
+++ lib/bitcoin_payable/bitcoin_payment_transaction.rb
@@ -5,11 +5,6 @@ module BitcoinPayable
 
     before_save :add_conversion
 
-    def self.format_transaction(transaction, recepient_address)
-      adapter = BitcoinPayable::Adapters::Base.fetch_adapter
-      adapter.convert_transactions(transaction, recepient_address)
-    end
-
     def add_conversion
       adapter = BitcoinPayable::Adapters::Base.fetch_adapter
       self.btc_conversion ||= bitcoin_payment.btc_conversion
