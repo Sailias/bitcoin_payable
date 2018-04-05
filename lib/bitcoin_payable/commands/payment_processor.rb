a0f44170ac3110dc5113c54822841c013727e5d3
Jonathan Salis <jon@voxpoplabs.com>
2018-04-04 22:36:29 -0400

    Defaulting payment processing range to 30 days

--- lib/bitcoin_payable/commands/payment_processor.rb
+++ lib/bitcoin_payable/commands/payment_processor.rb
@@ -5,22 +5,24 @@ module BitcoinPayable
     end
 
     def perform
-      BitcoinPayable::BitcoinPayment.where(state: [:pending, :partial_payment, :paid_in_full]).each do |payment|
-        begin
-          adapter = BitcoinPayable::Adapters::Base.fetch_adapter
+      BitcoinPayable::BitcoinPayment.
+        where(state: [:pending, :partial_payment, :paid_in_full]).
+        where("created_at > ?", 30.days.ago).each do |payment|
+          begin
+            adapter = BitcoinPayable::Adapters::Base.fetch_adapter
 
-          adapter.fetch_transactions_for_address(payment.address).each do |tx|
-            next if tx.nil?
+            adapter.fetch_transactions_for_address(payment.address).each do |tx|
+              next if tx.nil?
 
-            BitcoinPayable::Interactors::TransactionProcessor::Organizer.call(
-              bitcoin_payment: payment,
-              transaction: tx
-            )
+              BitcoinPayable::Interactors::TransactionProcessor::Organizer.call(
+                bitcoin_payment: payment,
+                transaction: tx
+              )
+            end
+          rescue JSON::ParserError
+            puts "Error processing response from server.  Possible API issue or your Quota has been exceeded"
           end
-        rescue JSON::ParserError
-          puts "Error processing response from server.  Possible API issue or your Quota has been exceeded"
         end
-      end
     end
   end
 end
