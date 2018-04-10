9a7a59b06f5ffabd094e1b9f1e40e1c4ac501c66
Guillermo <maesitos@gmail.com>
2018-04-10 22:20:39 +0200

    make sure the exchange rate is present in the database for a new fiat

--- lib/bitcoin_payable/currency_conversion.rb
+++ lib/bitcoin_payable/currency_conversion.rb
@@ -1,5 +1,11 @@
 module BitcoinPayable
   class CurrencyConversion < ::ActiveRecord::Base
     validates :btc, presence: true
+
+    def self.last_rate_for(currency)
+      PricingProcessor.new(currency: currency).perform unless self.where(currency: currency).any?
+      self.where(currency: currency).last.btc
+    end
+
   end
 end
