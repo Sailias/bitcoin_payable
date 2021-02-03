f835e349c36564ea6721520eeeb62a6d04588744
ispirett <ispirett@gmail.com>
2021-02-03 00:14:24 -0400

    fix(has_bitcoin_payment): changed class to string

--- lib/bitcoin_payable/has_bitcoin_payment.rb
+++ lib/bitcoin_payable/has_bitcoin_payment.rb
@@ -8,7 +8,7 @@ module BitcoinPayable
     module ClassMethods
 
       def has_bitcoin_payments(options = {})
-        has_many :bitcoin_payments, -> {order(:id)}, class_name: BitcoinPayable::BitcoinPayment, as: 'payable'
+        has_many :bitcoin_payments, -> {order(:id)}, class_name: "BitcoinPayable::BitcoinPayment", as: 'payable'
       end
 
     end
