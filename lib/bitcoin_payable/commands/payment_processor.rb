6469ab2884e20e7c30c6a120d7ff9e3ecf4f7f6d
Jonathan Salis <jon@voxpoplabs.com>
2021-02-09 19:49:21 -0500

    Updating request delay to be float

--- lib/bitcoin_payable/commands/payment_processor.rb
+++ lib/bitcoin_payable/commands/payment_processor.rb
@@ -16,7 +16,7 @@ module BitcoinPayable
             # Cloudflare limits requests to 1200 req / 5 minutes
             # or 4 requests / 1 second
             # So we will sleep for 500ms after each request by default
-            sleep(BitcoinPayable.config.request_delay)
+            sleep(BitcoinPayable.config.request_delay.to_f)
           rescue JSON::ParserError
             puts "Error processing response from server.  Possible API issue or your Quota has been exceeded"
           end
--- lib/bitcoin_payable/version.rb
+++ lib/bitcoin_payable/version.rb
@@ -1,3 +1,3 @@
 module BitcoinPayable
-  VERSION = "0.7.49"
+  VERSION = "0.7.50"
 end
