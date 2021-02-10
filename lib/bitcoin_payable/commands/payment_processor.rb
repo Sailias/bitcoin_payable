aedf7f9b854efb392f0da530db5af645fa7c94be
Jonathan Salis <jon@voxpoplabs.com>
2021-02-09 19:58:19 -0500

    Making API requests back off on error

--- lib/bitcoin_payable/commands/payment_processor.rb
+++ lib/bitcoin_payable/commands/payment_processor.rb
@@ -8,18 +8,23 @@ module BitcoinPayable
       BitcoinPayable::BitcoinPayment.
         where(state: [:pending, :partial_payment, :paid_in_full]).
         where("created_at > ?", (BitcoinPayable.config.processing_days.to_i).days.ago).each do |payment|
+          sleep_amount = BitcoinPayable.config.request_delay.to_f
+
           begin
             BitcoinPayable::Interactors::BitcoinPaymentProcessor::ProcessTransactionsForPayment.call(
               payment: payment
             )
           rescue JSON::ParserError
+            # Back off on error
+            sleep_amount = sleep_amount * 5
+
             puts "Error processing response from server.  Possible API issue or your Quota has been exceeded"
           end
 
           # Cloudflare limits requests to 1200 req / 5 minutes
           # or 4 requests / 1 second
           # So we will sleep for 500ms after each request by default
-          sleep(BitcoinPayable.config.request_delay.to_f)
+          sleep(sleep_amount)
         end
     end
   end
