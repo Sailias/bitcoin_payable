330b82e3e428841732c732080e9ef6dd50f83af8
Jonathan Salis <jon@voxpoplabs.com>
2021-02-09 19:57:24 -0500

    Sleep even after error

--- lib/bitcoin_payable/commands/payment_processor.rb
+++ lib/bitcoin_payable/commands/payment_processor.rb
@@ -12,14 +12,14 @@ module BitcoinPayable
             BitcoinPayable::Interactors::BitcoinPaymentProcessor::ProcessTransactionsForPayment.call(
               payment: payment
             )
-
-            # Cloudflare limits requests to 1200 req / 5 minutes
-            # or 4 requests / 1 second
-            # So we will sleep for 500ms after each request by default
-            sleep(BitcoinPayable.config.request_delay.to_f)
           rescue JSON::ParserError
             puts "Error processing response from server.  Possible API issue or your Quota has been exceeded"
           end
+
+          # Cloudflare limits requests to 1200 req / 5 minutes
+          # or 4 requests / 1 second
+          # So we will sleep for 500ms after each request by default
+          sleep(BitcoinPayable.config.request_delay.to_f)
         end
     end
   end
